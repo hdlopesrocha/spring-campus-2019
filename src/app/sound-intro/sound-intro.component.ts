a06ddfb098ba5c3e9307f83768d40e183a179bfc
Henrique Rocha <hdlopesrocha91@gmail.com>
2019-05-30 18:25:50 +0200

    Add wave animation

--- src/app/sound-intro/sound-intro.component.ts
+++ src/app/sound-intro/sound-intro.component.ts
@@ -7,19 +7,78 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 })
 export class SoundIntroComponent implements OnInit, OnDestroy {
 
+  canvas = null;
+  points = [];
+  pointSize = 3;
+  hDistance = 10;
+  vDistance = this.hDistance*Math.sqrt(3)/2.0;
+  amplitude = 10;
+  time = 0;
+  lastRender = 0;
+
   constructor() {
 
   }
 
   ngOnInit() {
+    this.canvas = document.getElementById("canvas");
+
+
+    for(var y= 0; y*this.vDistance < this.canvas.height; ++y ) {
+      for(var x= 0; x*this.hDistance < this.canvas.width; ++x) {
+        this.points.push({
+          x:this.hDistance*(y%2 ? x : x+0.5),
+          y:this.vDistance*y
+        });
+      }
+    }
+
+    window.requestAnimationFrame(this.loop.bind(this));
   }
 
   ngOnDestroy(): void {
   }
 
 
+  draw() {
+    const ctx = this.canvas.getContext("2d");
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.fillStyle = "#000000";
 
+    for(var i=0 ; i < this.points.length ; ++i) {
+      const cx = this.canvas.width/2;
+      const cy = this.canvas.height/2;
+      const px = this.points[i].x;
+      const py = this.points[i].y;
+      const dx = cx-px;
+      const dy = cy-py;
 
+      const r = Math.sqrt(dx*dx+dy*dy);
+      const a = Math.atan2(dy,dx);
+      const t = r*0.05-this.time*0.003;
+
+      const displacement = this.amplitude*(2+Math.sin(t)/(r*r*0.00001+0.5));
+
+      const x= cx + (r+displacement)*Math.cos(a);
+      const y= cy + (r+displacement)*Math.sin(a);
+
+      ctx.beginPath(); //Start path
+      ctx.arc(x, y, this.pointSize, 0, Math.PI * 2, true);
+      ctx.fill();
+    }
+  }
+
+  update(progress){
+    this.time += progress;
+  }
+
+  loop(timestamp) {
+    const progress = timestamp - this.lastRender;
+    this.update(progress);
+    this.draw();
+    this.lastRender = timestamp;
+    window.requestAnimationFrame(this.loop.bind(this));
+  }
 
 
 }
