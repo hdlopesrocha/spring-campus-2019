30b8ffb8e9fb1316f8081c2037f716c8458ba37b
Henrique Rocha <hdlopesrocha91@gmail.com>
2019-09-16 11:26:53 +0200

    Add ADSR envelope

---
+++ src/app/sound-envelope/sound-envelope.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sound-envelope',
+  templateUrl: './sound-envelope.component.html',
+  styleUrls: ['./sound-envelope.component.scss']
+})
+export class SoundEnvelopeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
