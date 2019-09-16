e275ee2451b49547094525479bbe11e764de53f2
Henrique Rocha <hdlopesrocha91@gmail.com>
2019-09-16 10:57:10 +0200

    Add music references

--- src/app/sound-test/sound-test.component.ts
+++ src/app/sound-test/sound-test.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoundTestComponent implements OnInit {
 
+
+  formula: string = "f(t)=\\frac{4\\sin(7\\times 2\\pi t)}{7 \\pi}+\\frac{4\\sin(3\\times 2\\pi t)}{3 \\pi}+\\frac{4\\sin(5\\times 2\\pi t)}{5 \\pi}";
+  showAnswer: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  checkAnswer() {
+    this.showAnswer = true;
+  }
 }
