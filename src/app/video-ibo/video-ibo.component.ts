581230e45ca49fdef3447f8da03082269054f1ea
Henrique Rocha <hdlopesrocha91@gmail.com>
2019-09-18 14:44:59 +0200

    Last commit

--- src/app/video-ibo/video-ibo.component.ts
+++ src/app/video-ibo/video-ibo.component.ts
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-ibo.component.scss']
 })
 export class VideoIboComponent implements OnInit {
+  code = "// WITHOUT INDEXING\n" +
+    "SingleVertex = [\n" +
+    "  // first  triangle\n" +
+    "  0, 0,\n" +
+    "  0, 1,\n" +
+    "  1, 0,\n" +
+    "  // second triangle\n" +
+    "  1, 0,\n" +
+    "  0, 1,\n" +
+    "  1, 1\n" +
+    "];\n" +
+    "\n" +
+    "// WITH INDEXING\n" +
+    "VertexBuffer = [\n" +
+    "  0, 0,\n" +
+    "  0, 1,\n" +
+    "  1, 0,\n" +
+    "  1, 1\n" +
+    "];\n" +
+    "\n" +
+    "IndexBuffer = [\n" +
+    "  // first  triangle\n" +
+    "  0, 1, 2\n" +
+    "  // second triangle\n" +
+    "  2, 1, 3\n" +
+    "];";
 
   constructor() { }
 
